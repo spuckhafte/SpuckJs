5c823e0b0695f06b3b3286a08cd4e1baa09b4ce1
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-05-02 12:26:54 +0530

    ::$effect()::
    use effect type functionality added

--- Spuck.js
+++ Spuck.js
@@ -6,6 +6,8 @@ class Spuck {
         this.attr = attr; // {} value:_, placeholder:_, etc.
         this._state = {}; // { stateName: [stateValue, changeStateFunction] }
         this._pseudoState = {}; // { stateName: [stateValue, changeStateFunction] }
+        this.#_effects = {}; // { 1: [effectFunc, [dep]] }
+        this.#_deps = {}; // { '$-state': [value, firstTimeOrNot] }
     }
  
     #_SP = '$-' // state prefix
@@ -13,6 +15,8 @@ class Spuck {
 
     _state // states of the elements
     _pseudoState // states of the pseudo-parent elements (pseudo states), if any
+    #_effects // functions that run when change occurs in some state
+    #_deps // all the states that are triggering some effects
 
     render(query) { // creates or updates an element
         // query -> argument to trigger re-rendering, if need, it just updates the properties of the existing element
@@ -23,7 +27,7 @@ class Spuck {
         this.init.id && !this.check(this.init.id) && el.setAttribute('id', this.init.id); // add id
 
         // add id if it's set as some state's name by extracting it
-        if (this.init.id && this.check(this.init.id)) el.setAttribute('id', this.changeVal(this.init.id));
+        if (this.init.id && this.check(this.init.id)) el.setAttribute('id', this.formatString(this.init.id));
 
         // set pseudo-states of the pesudo-children if any 
         this.init.pseudoChildren && this.init.pseudoChildren.forEach(child => {
@@ -34,7 +38,7 @@ class Spuck {
         if (this.attr && Object.keys(this.attr).length > 0) { 
             for (let key in this.attr) {
                 if (!this.check(this.attr[key])) el.setAttribute(key, this.attr[key]);
-                else el.setAttribute(key, this.changeVal(this.attr[key]));
+                else el.setAttribute(key, this.formatString(this.attr[key]));
             }
         }
 
@@ -42,16 +46,16 @@ class Spuck {
         if (this.prop) {
             if (this.prop.text) {
                 if (!this.check(this.prop.text)) el.innerHTML = this.prop.text;
-                else el.innerHTML = this.changeVal(this.prop.text);
+                else el.innerHTML = this.formatString(this.prop.text);
             }
             if (this.prop.value) {
                 if (!this.check(this.prop.value)) el.value = this.prop.value;
-                else el.value = this.changeVal(this.prop.value);
+                else el.value = this.formatString(this.prop.value);
             }
             if (this.prop.css) {
                 for (let style of Object.keys(this.prop.css)) {
                     if (!this.check(this.prop.css[style])) el.style[style] = this.prop.css[style];
-                    else el.style[style] = this.changeVal(this.prop.css[style]);
+                    else el.style[style] = this.formatString(this.prop.css[style]);
                 }
             }
         }
@@ -61,6 +65,29 @@ class Spuck {
             for (let event of Object.keys(this.events)) {
                 el.addEventListener(event, this.events[event]);
             }
+        }
+
+
+        if (Object.keys(this.#_deps).length > 0) { // run effect functions if dependencies change
+            let dependencies = Object.keys(this.#_deps);
+            let alteredDeps = []
+            dependencies.forEach(depend => {
+                if (this.#_deps[depend][0] !== this.changeVal(depend) || this.#_deps[depend][1]) {
+                    if (this.#_deps[depend][1]) this.#_deps[depend][1] = false;
+                    else this.#_deps[depend][0] = this.changeVal(depend);
+                    alteredDeps.push(depend)
+                }
+            })
+            
+            Object.keys(this.#_effects).forEach(_effectIndex => {
+                let effect = this.#_effects[_effectIndex];
+                for (let dep of effect[1]) {
+                    if (alteredDeps.includes(dep)) {
+                        effect[0]();
+                        break;
+                    }
+                }
+            })
         }
 
         return this
@@ -94,7 +121,7 @@ class Spuck {
         return this._state[_name][0]
     }
 
-    getPseudoState(_name) { // gets the current value of a particular pseudo state
+    getPseudoState(_name) { // gets the current value of a particular pseudo state]
         return this._pseudoState[_name][0]
     }
 
@@ -115,7 +142,21 @@ class Spuck {
         if (val.startsWith(this.#_CSP)) return this.getPseudoState(this.#_getStateName(val));
     }
 
+    formatString(text) { // formats a string, converting any $-state to its value
+        if (!text.includes(this.#_SP) && !text.includes(this.#_CSP)) return text;
+        let formatted = ''
+        if (text.split(' ').length === 1) return this.changeVal(text)
+        text.split(' ').forEach(tx => formatted += this.check(tx) ? `${this.changeVal(tx)} ` : `${tx} `)
+        return formatted
+    }
+
     check(query) { // checks if query is a prefixed state name
-        return query.toString().startsWith(this.#_SP) || query.toString().startsWith(this.#_CSP)
+        return query.toString().includes(this.#_SP) || query.toString().includes(this.#_CSP)
+    }
+
+    $effect(_func, _deps) { // run an effect (function) when one of the dependency (state) change
+        let effectIndex = Object.keys(this.#_effects).length + 1;
+        _deps.forEach(_dep => this.#_deps[_dep] = [this.formatString(_dep), true])
+        this.#_effects[effectIndex] = [_func, _deps]
     }
 }
